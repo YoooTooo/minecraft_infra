20f69ce5396646cdada71a7eba9871adcbfe38b0
YoooTooo <son.toyo.mcmxc@gmail.com>
2022-08-06 14:37:08 +0900

    セキュリティグループ

---
+++ aws_security_group.tf
@@ -0,0 +1,16 @@
+resource "aws_security_group" "allow_all" {
+  name = "allow_ssh_and_tcp"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  description = "allow tcp 22 and 80"
+}
--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,11 @@
 resource "aws_instance" "minecraft" {
   count         = 1
   ami           = "ami-785c491f" # Ubuntu 16.04 LTS official ami
+  key_name               = aws_key_pair.key_pair.id
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   tags = {
-    Name = "${format("minecraft-%02d", count.index + 1)}"
+    Name = "${format("minecraft-%02d", count.index)}"
   }
 }
