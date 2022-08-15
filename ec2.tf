2fc175f03b7f63d0782ee13856ef38a0118f5cd2
YoooTooo <son.toyo.mcmxc@gmail.com>
2022-08-15 19:16:25 +0900

    amazon linuxに変更

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "minecraft" {
   count         = 1
-  ami           = "ami-785c491f" # Ubuntu 16.04 LTS official ami
+  ami           = "ami-0ecb2a61303230c9d" #Amazon Linux2
   key_name               = aws_key_pair.key_pair.id
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
